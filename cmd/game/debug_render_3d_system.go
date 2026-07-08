2f8c3d8ae949eca6f661a432a476908bf57b3041
Gary Popov <popov.gary@gmail.com>
2026-07-08 18:57:09 -0500

    Make the debug render system toggleable

--- cmd/game/debug_render_3d_system.go
+++ cmd/game/debug_render_3d_system.go
@@ -2,11 +2,23 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-type DebugRender3DSystem struct{}
+type DebugRender3DSystem struct {
+	overlayActive bool
+}
 
-func (system *DebugRender3DSystem) Initialize(game *Game) {}
+func (system *DebugRender3DSystem) Initialize(game *Game) {
+	system.overlayActive = true
+}
 
 func (system *DebugRender3DSystem) Update(game *Game) {
+	if rl.IsKeyPressed(rl.KeyF10) {
+		system.overlayActive = !system.overlayActive
+	}
+
+	if !system.overlayActive {
+		return
+	}
+
 	rl.BeginMode3D(game.camera)
 	system.drawCoordinateSystemAt(rl.NewVector3(0, 0, 0))
 	rl.EndMode3D()

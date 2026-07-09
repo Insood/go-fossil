b455b1a5f70b24d514a2790327acf838f9b92bd9
Gary Popov <popov.gary@gmail.com>
2026-07-09 18:45:20 -0500

    DebugRender3D system casts a light & drone ray

--- cmd/game/debug_render_3d_system.go
+++ cmd/game/debug_render_3d_system.go
@@ -1,10 +1,19 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	ecs "github.com/mlange-42/ark/ecs"
+)
 
-type DebugRender3DSystem struct{}
+type DebugRender3DSystem struct {
+	droneFilter *ecs.Filter2[Position3, Drone]
+	lightFilter *ecs.Filter1[Light]
+}
 
-func (system *DebugRender3DSystem) Initialize(game *Game) {}
+func (system *DebugRender3DSystem) Initialize(game *Game) {
+	system.droneFilter = ecs.NewFilter2[Position3, Drone](game.world)
+	system.lightFilter = ecs.NewFilter1[Light](game.world)
+}
 
 func (system *DebugRender3DSystem) Update(game *Game) {
 	if !debugOverlayVisible {
@@ -13,6 +22,8 @@ func (system *DebugRender3DSystem) Update(game *Game) {
 
 	rl.BeginMode3D(game.camera)
 	system.drawCoordinateSystemAt(rl.NewVector3(0, 0, 0))
+	system.drawDroneGroundRay()
+	system.drawLightGuide()
 	rl.EndMode3D()
 }
 
@@ -20,4 +31,33 @@ func (system *DebugRender3DSystem) drawCoordinateSystemAt(origin rl.Vector3) {
 	rl.DrawLine3D(origin, rl.Vector3Add(origin, rl.NewVector3(axisLength, 0, 0)), rl.Red)
 	rl.DrawLine3D(origin, rl.Vector3Add(origin, rl.NewVector3(0, axisLength, 0)), rl.Green)
 	rl.DrawLine3D(origin, rl.Vector3Add(origin, rl.NewVector3(0, 0, axisLength)), rl.Blue)
+}
+
+func (system *DebugRender3DSystem) drawDroneGroundRay() {
+	query := system.droneFilter.Query()
+	defer query.Close()
+
+	if !query.Next() {
+		return
+	}
+
+	position, _ := query.Get()
+	start := rl.Vector3(*position)
+	end := rl.NewVector3(start.X, 0, start.Z)
+
+	rl.DrawLine3D(start, end, rl.Yellow)
+	rl.DrawSphere(end, 0.08, rl.Orange)
+}
+
+func (system *DebugRender3DSystem) drawLightGuide() {
+	query := system.lightFilter.Query()
+	defer query.Close()
+
+	if !query.Next() {
+		return
+	}
+
+	light := query.Get()
+	rl.DrawLine3D(light.origin, light.target, rl.SkyBlue)
+	rl.DrawSphere(light.origin, 0.1, rl.Blue)
 }

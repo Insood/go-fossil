76489742db785a0ce6c40ed401647f832275f56b
Gary Popov <popov.gary@gmail.com>
2026-07-25 11:25:48 -0500

    move clampInt

--- cmd/game/terrain_chunk.go
+++ cmd/game/terrain_chunk.go
@@ -193,13 +193,3 @@ func (chunk *TerrainChunk) uploadArtifactImageRect(rect image.Rectangle) {
 		pixels,
 	)
 }
-
-func clampInt(value, minValue, maxValue int) int {
-	if value < minValue {
-		return minValue
-	}
-	if value > maxValue {
-		return maxValue
-	}
-	return value
-}
--- cmd/game/utils.go
+++ cmd/game/utils.go
@@ -38,3 +38,13 @@ func clampFloat32(value, min, max float32) float32 {
 func xzVector(position rl.Vector3) rl.Vector2 {
 	return rl.NewVector2(position.X, position.Z)
 }
+
+func clampInt(value, minValue, maxValue int) int {
+	if value < minValue {
+		return minValue
+	}
+	if value > maxValue {
+		return maxValue
+	}
+	return value
+}
